ecc5b011b18ea37b9a9ae611798da4e2e4fd351b
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-07-23 01:39:19 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,18 +1,80 @@
 import React, { useState } from "react";
-// Navigation
+import { Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { useRoute } from "./router";
-// Custom Fonts Loader
-import useCustomFonts from "./hooks/getCustomFonts";
+import { createStackNavigator } from "@react-navigation/stack";
+import "react-native-gesture-handler";
+import { useFonts } from "expo-font";
+import { StatusBar } from "expo-status-bar";
+
+import RegistrationScreen from "./Screens/RegistrationScreen";
+import LoginScreen from "./Screens/LoginScreen";
+import Home from "./Screens/Home";
+import MapScreen from "./Screens/MapScreen";
+import CommentsScreen from "./Screens/CommentsScreen";
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  const fontsLoaded = useCustomFonts();
+  const [fontsLoaded] = useFonts({
+    JosefinSansThin: require("./assets/fonts/Josefin_Sans/static/JosefinSans-Thin.ttf"),
+    JosefinSansRegular: require("./assets/fonts/Josefin_Sans/static/JosefinSans-Regular.ttf"),
+    JosefinSansBold: require("./assets/fonts/Josefin_Sans/static/JosefinSans-Bold.ttf"),
+  });
 
   if (!fontsLoaded) {
     return null;
   }
 
-  const routing = useRoute(null);
-
-  return <NavigationContainer>{routing}</NavigationContainer>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Registration"
+          component={RegistrationScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Comments"
+          component={CommentsScreen}
+          options={{ title: "Comments" }}
+        />
+        <Stack.Screen
+          name="Map"
+          component={MapScreen}
+          options={{ title: "Location" }}
+        />
+      </Stack.Navigator>
+      <StatusBar style="auto" />
+    </NavigationContainer>
+  );
 }
+
+// !==============
+// import React, { useState } from "react";
+// // Navigation
+// import { NavigationContainer } from "@react-navigation/native";
+// import { useRoute } from "./router";
+// // Custom Fonts Loader
+// import useCustomFonts from "./hooks/getCustomFonts";
+
+// export default function App() {
+//   const fontsLoaded = useCustomFonts();
+
+//   if (!fontsLoaded) {
+//     return null;
+//   }
+
+//   const routing = useRoute(null);
+
+//   return <NavigationContainer>{routing}</NavigationContainer>;
+// }
