bea8186119644e0822b812dde7cc8df12e75cba7
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-07-26 23:39:16 +0300

    update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -8,14 +8,15 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "api-key", // my API key
-  authDomain: "project-id.firebaseapp.com", // my domain name
-  databaseURL: "https://project-id.firebaseio.com", // url
-  projectId: "project-id", //
-  storageBucket: "project-id.appspot.com", //
-  messagingSenderId: "sender-id", //
-  appId: "app-id", //
-  measurementId: "G-measurement-id", //
+  apiKey: "AIzaSyBC15CY8KeE6hwLjG5sWIa39IssQYkkGlo",
+  authDomain: "socnetapp-ae36c.firebaseapp.com",
+  databaseURL:
+    "https://socnetapp-ae36c-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "socnetapp-ae36c",
+  storageBucket: "socnetapp-ae36c.appspot.com",
+  messagingSenderId: "462755268720",
+  appId: "1:462755268720:web:e94a51dbda0b9ec8344cb5",
+  measurementId: "G-HEZ2SWWRLQ",
 };
 
 // Initialize Firebase
