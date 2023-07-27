eafa05e1e0dd0e483b27276988220fd39e1af97a
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-07-27 21:23:13 +0300

    Update App.js

--- App.js
+++ App.js
@@ -3,8 +3,7 @@ import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { useRoute } from "./router";
 // Custom Fonts Loader
-// import useCustomFonts from "./hooks/getCustomFonts";
-import { useFonts } from "expo-font";
+import useCustomFonts from "./hooks/getCustomFonts";
 // status bar - time, network status, battery charge, wireless networks
 import { StatusBar } from "expo-status-bar";
 // redux
@@ -13,12 +12,7 @@ import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "./redux/store";
 
 export default function App() {
-  // const { fontsLoaded } = useCustomFonts();
-  const [fontsLoaded] = useFonts({
-    JosefinSansThin: require("./assets/fonts/Josefin_Sans/static/JosefinSans-Thin.ttf"),
-    JosefinSansRegular: require("./assets/fonts/Josefin_Sans/static/JosefinSans-Regular.ttf"),
-    JosefinSansBold: require("./assets/fonts/Josefin_Sans/static/JosefinSans-Bold.ttf"),
-  });
+  const { fontsLoaded } = useCustomFonts();
 
   const routing = useRoute(false);
 
@@ -41,17 +35,3 @@ export default function App() {
     </>
   );
 }
-
-// !==============
-
-// ! for load fonts from hook, dont working
-// const { fontsLoaded, setFontsLoaded, loadFonts } = useCustomFonts();
-// if (!fontsLoaded) {
-//   return (
-//     <AppLoading
-//       startAsync={loadFonts}
-//       onFinish={() => setFontsLoaded(true)}
-//       onError={console.warn}
-//     />
-//   );
-// }
